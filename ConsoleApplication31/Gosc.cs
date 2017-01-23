3c6461ceecb312c748e0582b220beec7e030a8ea
Piotrekk85 <piotr.jastr@gmail.com>
2017-01-23 20:19:06 +0100

    Add files via upload

---
+++ ConsoleApplication31/Gosc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication31
+{
+    class Gosc:IComparable<Gosc>
+    {
+        private string imie;
+        private string nazwisko;
+
+        public Gosc(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+
+        public override string ToString()
+        {
+            return "Imię i nazwisko: " + imie + " " + nazwisko;
+        }
+    
+        public int CompareTo(Gosc other)
+        {
+ 	        return this.nazwisko.CompareTo(other.nazwisko);
+        }
+    }
+}
---
+++ ConsoleApplication31/Pokoj.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication31
+{
+    class Pokoj
+    {
+        private int nrPokoju;
+        private double cenaZaDzien;
+
+        public Pokoj(int nr, double cena)
+        {
+            this.nrPokoju = nr;
+            this.cenaZaDzien = cena;
+        }
+
+        public override string ToString()
+        {
+            return "Pokój: " + nrPokoju + ", cena za dzień: " + cenaZaDzien;
+        }
+
+        public int CompareTo(Pokoj other)
+        {
+            return this.nrPokoju.CompareTo(other.nrPokoju);
+        }
+
+    }
+}
